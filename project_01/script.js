fdd22de214605bb3ffb3f61bae9b31e8492968c1
Evgrafova.Lera <evgrafova.valerie@mail.ru>
2022-11-23 17:39:50 +0300

    выполнены условия для второй части задания

--- project_01/script.js
+++ project_01/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -22,9 +22,9 @@ const personalMovieDB = {
 
 for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?');
+          b = prompt('На сколько оцените его?', '');
 
-    if (a != null && b != null && a != '' && b != '' && a.length > 50) {
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
         personalMovieDB.movies[a] = b;
         console.log('done');
     } else {
@@ -32,6 +32,17 @@ for (let i = 0; i < 2; i++) {
         i--;
     }
 }
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+
 
 console.log(personalMovieDB);
 
