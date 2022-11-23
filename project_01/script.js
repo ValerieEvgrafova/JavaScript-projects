755d92ffcc12d266bfc010c1ed97625cb121f4b3
Evgrafova.Lera <evgrafova.valerie@mail.ru>
2022-11-23 17:03:03 +0300

    Выполнена вторая часть практического задания

--- project_01/script.js
+++ project_01/script.js
@@ -1,4 +1,6 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+'use strict';
+
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,12 +10,28 @@ const personalMovieDB = {
     privat: false
 };
 
-let a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?'),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?');
+// let a = prompt('Один из последних просмотренных фильмов?', ''),
+//     b = prompt('На сколько оцените его?'),
+//     c = prompt('Один из последних просмотренных фильмов?', ''),
+//     d = prompt('На сколько оцените его?');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// Код переписан ниже с использованием цикла
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?');
+
+    if (a != null && b != null && a != '' && b != '' && a.length > 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
 
 console.log(personalMovieDB);
+
