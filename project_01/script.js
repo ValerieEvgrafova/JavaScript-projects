50c79f8cff62a23476c46ee54f3aed43c0230650
Evgrafova.Lera <evgrafova.valerie@mail.ru>
2023-03-27 17:26:39 +0300

    was added new function

--- project_01/script.js
+++ project_01/script.js
@@ -76,7 +76,7 @@ const personalMovieDB = {
 
         personalMovieDB.genres.forEach((item, i) => {
             console.log(`Любимый жанр ${i + 1} - это ${item}`);
-        })
+        });
     }
 };
 
